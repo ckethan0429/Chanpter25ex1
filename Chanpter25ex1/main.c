55071206d6cdaca796670df071a192c5220e1ec4
안창균 <oops0429@gmail.com>
2019-01-30 23:46:18 +0900

    Initial Commit

---
+++ Chanpter25ex1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chanpter25ex1
+//
+//  Created by mac_ck on 30/01/2019.
+//  Copyright © 2019 mac_ck. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
